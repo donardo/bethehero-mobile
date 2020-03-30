4fb50010937651367ca78e58eaca85fbdd349baa
Douglas Donardo <douglasddd@gmail.com>
2020-03-30 17:00:25 -0300

    Ajustes na Aplicação

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.104:3333'
+    baseURL: 'https://app-bethehero-back.herokuapp.com'
+    //baseURL: 'http://192.168.0.104:3333'
 });
 
 export default api;
